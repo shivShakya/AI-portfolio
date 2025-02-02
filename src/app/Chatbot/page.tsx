ff3c153ee7d8ecb96f4b9043e298b3dd84330feb
shivamMRARVR <shivam@mrarvr.in>
2025-02-03 01:21:44 +0530

    7th commit

--- src/app/Chatbot/page.tsx
+++ src/app/Chatbot/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useState } from "react";
 import TextBot from "./TextBot/page";
 
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Link from "next/link";
 import Tilt from 'react-parallax-tilt';
 
